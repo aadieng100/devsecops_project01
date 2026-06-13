88528daf3458b203aa08da1e9b534a1a220ab304
abdouladieng <diengabdoulaziz110@gmail.com>
2026-06-13 13:00:56 +0000

    sec: add inline semgrep bypass for explicit staging EC2 public IP

--- terraform/main.tf
+++ terraform/main.tf
@@ -101,7 +101,9 @@ resource "aws_instance" "app_server" {
   instance_type = "t2.micro"             # Free-tier eligible tiny instance
   subnet_id     = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.app_sg.id]
-associate_public_ip_address = true # SECURE: Explicitly assign a public IP ONLY to this application container
+  
+  # nosemgrep: terraform.aws.security.aws-ec2-has-public-ip.aws-ec2-has-public-ip
+  associate_public_ip_address = true
 
   tags = {
     Name        = "devsecops-app-server"
